c44cb81824cb71bc43ffd4f00ea8137075854ce8
Gerald Bishop <geraldbishop@Geralds-iMac.local>
2023-12-04 14:45:30 +0000

    Create tablePrinter.py
    Swaps columns with rows and prints a right-justified table

---
+++ 1_p154_table_printer/tablePrinter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# displays an organised table (that is right-justified) from a list of lists of strings
+
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+
+def max_length(table):
+    """list max length of strings"""
+    listMax = []
+    for j in range(len(table)):
+        lengths = []
+        for i in range(len(table[j])):
+            lengths.append(len(table[j][i]))
+        listMax.append(max(lengths))
+    return listMax
+
+
+def newTable(table):
+    """reorder lists"""
+    newTable = []
+    for i in range(len(table[0])):
+        myListA = []
+        for j in range(len(table)):
+            myListA.append(table[j][i])
+        newTable.append(myListA)
+    return newTable
+
+
+def main(table):
+    colWidths = max_length(table)
+    tableList = newTable(table)
+    for i in range(len(tableList)):
+        items = ""
+        for j in range(len(tableList[i])):
+            items += " " + tableList[i][j].rjust(colWidths[j])
+        print(items)
+
+
+if __name__ == "__main__":
+    main(tableData)
