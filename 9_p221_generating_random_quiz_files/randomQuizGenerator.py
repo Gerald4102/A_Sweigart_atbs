c765af37f0d9106a81f9765276303cbf4c3e5a9b
Gerald4102 <106319833+Gerald4102@users.noreply.github.com>
2023-12-16 14:45:20 +0000

    Create randomQuizGenerator.py

---
+++ 9_p221_generating_random_quiz_files/randomQuizGenerator.py
@@ -0,0 +1,69 @@
+#! python3
+# randomQuizGenerator.py - Creates quizzes with questions and answers in
+# random order, along with answer key.
+import random
+
+# The quiz data. Keys are English counties and values are the headquarters.
+hqs = {'Bedfordshire': 'Bedford', 
+       'Berkshire': 'Reading',
+       'Buckinghamshire': 'Aylesbury',
+       'Dorset': 'Dorchester',
+       'Cambridgeshire': 'Cambridge',
+       'Cambridgeshire': 'March',
+       'Cheshire': 'Chester ',
+       'Cornwall': 'Truro',
+       'County Durham': 'Durham',
+       'Cumberland': 'Carlisle',
+       'Derbyshire': 'Matlock',
+       'Devon': 'Exeter',
+       'East Riding of Yorkshire': 'Beverley',
+       'Essex': 'Chelmsford',
+       'Gloucestershire': 'Gloucester',
+       'Hampshire': 'Winchester',
+       'Hampshire': 'Newport',
+       'Herefordshire': 'Hereford',
+       'Hertfordshire': 'Hertford',
+       'Huntingdonshire': 'Huntingdon',
+       'Kent': 'Maidstone',
+       'Lancashire': 'Preston',
+       'Leicestershire': 'Glenfield',
+       'Lincolnshire': 'Boston',
+       'Lincolnshire': 'Sleaford',
+       'Lincolnshire': 'Lincoln',
+       'London': 'Lambeth',
+       'Middlesex': 'Westminster',
+       'Monmouthshire': 'Newport',
+       'Norfolk': 'Norwich',
+       'North Riding of Yorkshire': 'Northallerton',
+       'Northamptonshire': 'Northampton',
+       'Northamptonshire': 'Peterborough',
+       'Northumberland': 'Morpeth',
+       'Nottinghamshire': 'West Bridgford',
+       'Oxfordshire': 'Oxford',
+       'Rutland': 'Oakham',
+       'Shropshire': 'Shrewsbury',
+       'Somerset': 'Taunton',
+       'Staffordshire': 'Stafford',
+       'Suffolk': 'Ipswich',
+       'Suffolk': 'Bury St Edmunds',
+       'Surrey': 'Kingston upon Thames',
+       'Sussex': 'Lewes',
+       'Sussex': 'Chichester',
+       'Warwickshire': 'Warwick',
+       'Westmorland': 'Kendal',
+       'West Riding of Yorkshire': 'Wakefield',
+       'Wiltshire': 'Trowbridge',
+       'Worcestershire': 'Worcester'}
+
+for quizNum in range(35):
+    # Create the quiz and answer key files.
+    quizFile = open(f'countiesquiz{quizNum + 1}.txt', 'w')
+    answerFile = open(f'countiesquiz_answer{quizNum + 1}.txt', 'w')
+    # Write the header for the quiz.
+    quizFile.write('Name:\n\nDate:\n\n')
+    quizFile.write(' ' * 20 + f'English Counties HQ quiz (Form {quizNum + 1})')
+    quizFile.write('\n\n')
+
+    # Close the files.
+    quizFile.close()
+    answerFile.close()
